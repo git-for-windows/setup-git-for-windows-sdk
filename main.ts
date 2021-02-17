ebca0c370afa6931a7f1d712c1fb57fc050ccecb
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-02-17 19:38:07 +0100

    Skip this Action on non-Windows platforms
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 import * as core from '@actions/core'
+import process from 'process'
 import {get} from './src/downloader'
 
 async function run(): Promise<void> {
   try {
+    if (process.platform !== 'win32') {
+      core.warning(
+        `Skipping this Action because it only works on Windows, not on ${process.platform}`
+      )
+      return
+    }
     const flavor = core.getInput('flavor')
     const architecture = core.getInput('architecture')
 
