0a4939779e1488a9c2642081ae22e9d0956e3b5f
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-02-18 20:28:35 +0100

    Ensure that `prove` is found
    That's another thing `/etc/profile` would have done for us...
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- main.ts
+++ main.ts
@@ -52,7 +52,8 @@ async function run(): Promise<void> {
       }
     }
 
-    // Set up PATH so that Git for Windows' SDK's `bash.exe` is found
+    // Set up PATH so that Git for Windows' SDK's `bash.exe`, `prove` and `gcc` are found
+    core.addPath(`${outputDirectory}/usr/bin/core_perl`)
     core.addPath(`${outputDirectory}/usr/bin`)
     const msystem = architecture === 'i686' ? 'MING32' : 'MINGW64'
     core.addPath(`${outputDirectory}/${msystem.toLocaleLowerCase()}/bin`)
